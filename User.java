25df7e58d2f21fd932595c61e95a5c1e53dcdc74
qaid123 <qaidjamali53@gmail.com>
2023-04-28 20:30:29 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class User {
-    
+
     private String name;
     private String email;
     private String pass;
@@ -20,6 +20,38 @@ public class User {
         this.type = type;
     }
 
+    void setname(String name){
+        this.name=name;
+    }   
+    String getname(String name){
+        return name;
+    }
 
+    void setemail(String email){
+        this.email=email;
+    }   
+    String getemail(String email){
+        return email;
+    }
 
+    void setpass(String pass){
+        this.pass=pass;
+    }   
+    String getpass(String pass){
+        return pass;
+    }
+
+    void setphno(String phno){
+        this.phno=phno;
+    }   
+    String getphno(String phno){
+        return phno;
+    }
+
+    void settype(int type){
+        this.type=type;
+    }   
+    int gettype(int type){
+        return type;
+    }    
 }
