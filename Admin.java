769ce7c701c960694db1dcd32d8790f13b95729c
qaid123 <qaidjamali53@gmail.com>
2023-04-28 20:21:28 +0530

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,27 @@
+public class Admin extends User {
+    private String adminID;
+    private String adminpass;
+
+/*  Admin(String name, String email, String pass,String phno,String adminID,String adminpass){
+        this.name=name;
+        this.email=email;
+        this.pass=pass;
+        this.phno=phno;
+        this.adminID=adminID;
+        this.adminpass=adminpass;
+    }*/
+
+    public void set_adminID(String adminID) {
+        this.adminID=adminID;
+    }
+    public String get_adminID(String adminID) {
+        return adminID;
+    }
+
+    public void set_adminPass(String adminPass) {
+        this.adminPass=adminPass;
+    }
+    public String get_adminPass(String adminPass) {
+        return adminPass;
+    }
+}
